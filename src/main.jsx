85ed6bd90050e2a36c616fb333de4cd4d65a363f
mauriciolaser <mauriciocastrovaldez@gmail.com>
2025-03-16 13:05:09 +0200

    env boilerplate updated

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,25 @@ import ReactDOM from "react-dom";
 import App from "./App.jsx";
 import "./index.css"; // Aquí puedes agregar estilos globales si lo deseas
 
+// Configuración de Google Analytics 4
+if (import.meta.env.VITE_GA_ID) {
+  const gaId = import.meta.env.VITE_GA_ID;
+
+  // Agregar el script de Google Analytics dinámicamente
+  const script = document.createElement("script");
+  script.async = true;
+  script.src = `https://www.googletagmanager.com/gtag/js?id=${gaId}`;
+  document.head.appendChild(script);
+
+  script.onload = () => {
+    window.dataLayer = window.dataLayer || [];
+    function gtag(...args) { window.dataLayer.push(args); }
+
+    gtag('js', new Date());
+    gtag('config', gaId);
+  };
+}
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
