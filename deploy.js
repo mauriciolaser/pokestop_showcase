fcc8f4da53ee0032eeebdda8cb41f9647d25bda1
mauriciolaser <mauriciocastrovaldez@gmail.com>
2025-04-02 13:08:04 +0300

    arreglado el deploy

--- deploy.js
+++ deploy.js
@@ -1,17 +1,17 @@
-require('dotenv').config({ path: '.env.production' });
+const { DEPLOY_FTP_USER, DEPLOY_FTP_PASSWORD, DEPLOY_FTP_HOST, DEPLOY_FTP_PORT, DEPLOY_FTP_PATH } = require('./config');
 const FtpDeploy = require('ftp-deploy');
 const ftpDeploy = new FtpDeploy();
 
 const config = {
-  user: process.env.DEPLOY_FTP_USER,
-  password: process.env.DEPLOY_FTP_PASSWORD,
-  host: process.env.DEPLOY_FTP_HOST,
-  port: Number(process.env.DEPLOY_FTP_PORT),  // Aseguramos que sea numérico
-  localRoot: __dirname + "/dist",             // Aquí se asume que tu build se genera en /dist
-  remoteRoot: process.env.DEPLOY_FTP_PATH,      // Ruta remota definida en el .env.production
-  include: ["*", "**/*", ".*", "**/.*"],       // Incluye todos los archivos, incluidos los ocultos
-  deleteRemote: true,                          // Borra en remoto lo que no exista localmente
-  forcePasv: true                              // Fuerza el modo PASV
+  user: DEPLOY_FTP_USER,
+  password: DEPLOY_FTP_PASSWORD,
+  host: DEPLOY_FTP_HOST,
+  port: Number(DEPLOY_FTP_PORT), // Aseguramos que sea numérico
+  localRoot: __dirname + "/dist", // Aquí se asume que tu build se genera en /dist
+  remoteRoot: DEPLOY_FTP_PATH,     // Ruta remota definida en el archivo de configuración
+  include: ["*", "**/*", ".*", "**/.*"], // Incluye todos los archivos, incluidos los ocultos
+  deleteRemote: true,              // Borra en remoto lo que no exista localmente
+  forcePasv: true                  // Fuerza el modo PASV
 };
 
 ftpDeploy.deploy(config)
